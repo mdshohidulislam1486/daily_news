376c638fb3c6a3e76e5c8cebc376ff1c8f1de962
mdshohidulislam1486 <Shohidul Islam>
2022-04-23 23:56:03 +0600

    updated

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,5 +1,5 @@
 import { TrySharp } from '@mui/icons-material'
-import { Box, Container, Grid, Typography } from '@mui/material'
+import { Box, Button, Card, CardActions, CardContent, Container, Grid, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material'
 import React, { useEffect, useState } from 'react'
 import { setInterval } from 'timers/promises'
 import './Home.css'
@@ -11,20 +11,15 @@ interface NewsObject {
 
 const Home:React.FC = ()  => {
   const [news, setNews] = useState<any[]>([])
+  const [loding , setLoding] = useState(false)
+  const [currentPage, setCurrentPage] = useState<number>(1)
+  const [postPerPage, setPostPerPage] = useState<number>(10)
 
   useEffect(() => {
-    fetch('https://hn.algolia.com/api/v1/search_by_date?query=10')
-    .then(res =>{ if(!res.ok){
-      throw Error('No weather data found, please check again')
+    const fetchPosts = async () =>{
+      setLoding(true)
+      const res = await 
     }
-    return res.json()
-    })
-    .then(data =>{
-      setNews(data.hits)
-    }) 
-    .catch(err => {
-      console.log(err.message);
-    }); 
   
   }, [])
 
@@ -33,14 +28,33 @@ const Home:React.FC = ()  => {
   return (
     <Box className='home_bg'>
       <Typography variant='h3' sx={{textAlign:'center', fontWeight:600, py:5}}>Latest News</Typography>
-      <Container>
-        {
-            news?.map(n => (<Grid container key={n?.objectID}>
-              <Grid item>
-                {n.created_at.slice(0,10)}
-              </Grid>
-            </Grid>))
-        }
+      <Container sx={{display:"flex", flexWrap:'wrap', justifyContent:"center"}}>
+      <TableContainer>
+              <Table>
+                <TableHead>
+                  <TableRow data-aos="zoom-in-down">
+                  <TableCell><strong>Title</strong></TableCell>
+                  <TableCell><strong>Created at</strong></TableCell>
+                  <TableCell><strong>author</strong></TableCell>
+                  <TableCell><strong>Link Orginated</strong></TableCell>
+                  
+                  </TableRow>
+                </TableHead>
+
+               {news?.map((n) =>  <TableBody key={n._id}>
+                  <TableRow data-aos="zoom-in-down">
+                      <TableCell>{n?.story_title}</TableCell> 
+                      <TableCell>{n?.created_at.slice(0,10)}</TableCell> 
+                      <TableCell>{n?.author} </TableCell> 
+                      <TableCell sx={{cursor:'pointer', color:'#0000FF'}} ><a style={{textDecoration:'none'}} href={n?.story_url}><strong>Find Story Link</strong></a></TableCell>
+                     
+                      
+
+                  </TableRow>
+                </TableBody>)}
+              </Table>
+            </TableContainer>
+
       </Container>
     </Box>
   )
