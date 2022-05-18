ae8398fa7705e2385d35aebdf16226a257e849c9
mdshohidulislam1486 <Shohidul Islam>
2022-05-18 23:54:58 +0600

    upated

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -25,7 +25,6 @@ const Home:React.FC = ()  => {
   let myData = addNum + 1
    const fetchPosts = async () =>{
       const res = await axios.get(`https://hn.algolia.com/api/v1/search_by_date?tags=story&page=${addNum}`)
-      setNews(res.data.hits)
       setNews([...news, ...res.data.hits])
     }
    const id =  setInterval(()=>{
