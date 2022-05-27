434b594a6f5fe78499fba1cfdb0bae6cbd790cbd
mdshohidulislam1486 <Shohidul Islam>
2022-05-27 10:25:45 +0600

    updated

--- src/index.tsx
+++ src/index.tsx
@@ -10,9 +10,7 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  
     <Context><App/></Context>
- 
 );
 
 // If you want to start measuring performance in your app, pass a function
