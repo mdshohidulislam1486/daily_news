6c2ce088330c593c67352e7d872e34187278f407
mdshohidulislam1486 <Shohidul Islam>
2022-06-18 04:32:39 +0600

    updated

--- src/Home/Home.test.tsx
+++ src/Home/Home.test.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { render, screen } from '@testing-library/react';
 import Home from './Home';
 import {BrowserRouter as Router} from 'react-router-dom';
+import { MyNewsContext } from '../context/Context';
 
 
 
@@ -24,6 +25,8 @@ test('Chaeck the next button is disabled and enabled properly', async() =>{
   const listItems = screen.getAllByTestId('each-page')
   const previousButton =  screen.getByText(/next/i)
   expect(previousButton).toBeDisabled()
-  expect(listItems).toHaveLength(2)  
+  expect(listItems).toHaveLength(2)
+  const headLine = await screen.findByText(/Hoboken Hasn’t Had a Traffic Death in 4 Years. What’s Right?/i)
+  expect(headLine).toBeInTheDocument()
 
 })  
--- src/context/Context.test.tsx
+++ src/context/Context.test.tsx
@@ -20,28 +20,25 @@ configure({ adapter: new EnzymeAdapter() });
 
 
 
-describe("news lenght will be 20", ()=>{
-    interface children{
-        children:JSX.Element
-      }
+describe("<Context/> test", ()=>{
+  
 
-    it('should scroll into AnotherComponent when date is updated', () => {
+    it('get sample data', async() => {
         const mScrollIntoView = jest.fn();
         window.HTMLElement.prototype.scrollIntoView = mScrollIntoView;
-    
+    const news = [{"created_at":"2022-06-17T21:17:01.000Z","title":"Hoboken Hasn’t Had a Traffic Death in 4 Years. What’s Right?","url":"https://www.curbed.com/2022/06/hoboken-traffic-deaths-none-vision-zero-streets.html","author":"jseliger","points":1,"story_text":null,"comment_text":null,"num_comments":0,"story_id":null,"story_title":null,"story_url":null,"parent_id":null,"created_at_i":1655500621,"_tags":["story","author_jseliger","story_31784484"],"objectID":"31784484","_highlightResult":{"title":{"value":"Hoboken Hasn’t Had a Traffic Death in 4 Years. What’s Right?","matchLevel":"none","matchedWords":[]},"url":{"value":"https://www.curbed.com/2022/06/hoboken-traffic-deaths-none-vision-zero-streets.html","matchLevel":"none","matchedWords":[]},"author":{"value":"jseliger","matchLevel":"none","matchedWords":[]}}}]
         const { rerender } = render(
-          <MyNewsContext.Provider value={[]}>
+          <MyNewsContext.Provider value={news}>
             <Context children={<App/>} />
           </MyNewsContext.Provider>
         );
         expect(mScrollIntoView).not.toBeCalled();
     
         rerender(
-          <MyNewsContext.Provider value={[]}>
+          <MyNewsContext.Provider value={news}>
             <Context children={<App/>}  />
           </MyNewsContext.Provider>
         );
-        expect(mScrollIntoView).toBeCalled();
+        expect(mScrollIntoView).toBeCalledTimes(0);
       });
-
 })
